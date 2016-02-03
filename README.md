1a5ec375e315f085316426420f8e589056dcc7cf
Anton Babenko <anton@antonbabenko.com>
2016-02-03 21:31:57 +0100

    Added tf_aws_private_subnet_nat_gateway module

---
+++ README.md
@@ -0,0 +1,52 @@
+# Private subnets with NAT Gateway in VPC Terraform Module
+==========================================================
+
+A Terraform module to create private subnets with NAT Gateway in VPC in AWS.
+
+Make sure that you don't hit soft-limit of EIP per VPC.
+
+If you want to create public and private subnets using single module you can use [tf_aws_vpc module](https://github.com/terraform-community-modules/tf_aws_vpc).
+
+
+Module Input Variables
+----------------------
+
+- `name` - Name (optional)
+- `vpc_id` - VPC id
+- `cidrs` - Comma-separated list of private subnet CIDR blocks
+- `azs` - Comma-separated list of availability zones
+- `public_subnet_ids` - Comma-separated list of public subnet ids where NAT gateway will be created
+- `nat_gateways_count` - Number of NAT gateways to create (shoud be at least 1). For high-availability make it equal to public subnets.
+
+Usage
+-----
+
+```js
+module "private_subnet" {
+  source             = "github.com/terraform-community-modules/tf_aws_private_subnet_nat_gateway"
+
+  name               = "production-private"
+  vpc_id             = "vpc-12345678"
+  cidrs              = "10.4.1.0/24,10.4.2.0/24,10.4.3.0/24"
+  azs                = "eu-west-1a,eu-west-1b,eu-west-1c"
+  public_subnet_ids  = "subnet-123abcde,subnet-456abcde,subnet-789abcde"
+  nat_gateways_count = 3    # can be between 1 and "number of public subnets".
+}
+```
+
+Outputs
+=======
+
+- `subnet_ids` - Comma-separated list of private subnet ids
+- `private_route_table_ids` - Comma-separated list of route table ids
+- `nat_eips` - Comma-separated list of NAT gateways EIPs
+
+Authors
+=======
+
+Originally created and maintained by [Anton Babenko](https://github.com/antonbabenko).
+
+License
+=======
+
+Apache 2 Licensed. See LICENSE for full details.
---
+++ main.tf
@@ -0,0 +1,75 @@
+variable "name" {
+  default = "private"
+}
+variable "cidrs" {
+}
+variable "azs" {
+}
+variable "vpc_id" {
+}
+variable "public_subnet_ids" {
+}
+variable "nat_gateways_count" {
+}
+
+# Subnet
+resource "aws_subnet" "private" {
+  vpc_id            = "${var.vpc_id}"
+  cidr_block        = "${element(split(",", var.cidrs), count.index)}"
+  availability_zone = "${element(split(",", var.azs), count.index)}"
+  count             = "${length(split(",", var.cidrs))}"
+
+  tags {
+    Name = "${var.name}.${element(split(",", var.azs), count.index)}"
+  }
+}
+
+# Routes
+resource "aws_route_table" "private" {
+  vpc_id = "${var.vpc_id}"
+  count  = "${length(split(",", var.cidrs))}"
+
+  tags {
+    Name = "${var.name}.${element(split(",", var.azs), count.index)}"
+  }
+}
+
+resource "aws_route_table_association" "private" {
+  subnet_id      = "${element(aws_subnet.private.*.id, count.index)}"
+  route_table_id = "${element(aws_route_table.private.*.id, count.index)}"
+  count          = "${length(split(",", var.cidrs))}"
+}
+
+resource "aws_route" "nat_gateway" {
+  route_table_id         = "${element(aws_route_table.private.*.id, count.index)}"
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = "${element(aws_nat_gateway.nat.*.id, count.index)}"
+  count                  = "${length(split(",", var.cidrs))}"
+
+  depends_on             = ["aws_route_table.private"]
+}
+
+# NAT
+resource "aws_eip" "nat" {
+  vpc   = true
+  count = "${var.nat_gateways_count}"
+}
+
+resource "aws_nat_gateway" "nat" {
+  allocation_id = "${element(aws_eip.nat.*.id, count.index)}"
+  subnet_id     = "${element(split(",", var.public_subnet_ids), count.index)}"
+  count         = "${var.nat_gateways_count}"
+}
+
+# Output
+output "subnet_ids" {
+  value = "${join(",", aws_subnet.private.*.id)}"
+}
+
+output "private_route_table_ids" {
+  value = "${join(",", aws_route_table.private.*.id)}"
+}
+
+output "nat_eips" {
+  value = "${join(",", aws_eip.nat.*.public_ip)}"
+}
